face91a8209d84b71fabf546447cefbf6109d403
Marrani <gabrielmarrani@gmail.com>
2021-05-09 17:38:02 -0300

    Group by search

--- backend/src/main/java/com/marrani/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/marrani/dsvendas/service/SaleService.java
@@ -1,4 +1,6 @@
 package com.marrani.dsvendas.service;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -6,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.marrani.dsvendas.dto.SaleDTO;
+import com.marrani.dsvendas.dto.SaleSuccessDTO;
+import com.marrani.dsvendas.dto.SaleSumDTO;
 import com.marrani.dsvendas.entities.Sale;
 import com.marrani.dsvendas.repositories.SaleRepository;
 import com.marrani.dsvendas.repositories.SellerRepository;
@@ -24,6 +28,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 
 }
