bb3621dc56b584835810dc0db6c0dfa835d50e33
Marrani <gabrielmarrani@gmail.com>
2021-05-09 16:18:34 -0300

    Layers

---
+++ backend/src/main/java/com/marrani/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,42 @@
+package com.marrani.dsvendas.dto;
+
+import com.marrani.dsvendas.entities.Seller;
+
+public class SellerDTO {
+
+	private Long id;
+	private String name;
+
+	public SellerDTO() {
+
+	}
+
+	public SellerDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public SellerDTO(Seller entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ backend/src/main/java/com/marrani/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.marrani.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.marrani.dsvendas.dto.SellerDTO;
+import com.marrani.dsvendas.entities.Seller;
+import com.marrani.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
